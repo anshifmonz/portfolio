bd130a9893d3c69cb8753a0a2598e7944e6c4cd8
Muhammed Anshif <muhammedanshif236@gmail.com>
2024-11-05 10:20:53 +0530

    feat: send form submission to email and discord

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
+// faq
 const question = document.querySelectorAll('section.faq .item > p');
-question.forEach(ele => ele.addEventListener(e => toggleAnswer(e)))
+question.forEach(ele => ele.addEventListener('click', e => toggleAnswer(e)))
 
 function toggleAnswer(button) {
   const answer = button.nextElementSibling;
-  if (answer.style.display === "block") return answer.style.display = "none";
-  answer.style.display = "block";
+  if (answer.style.display === 'block') return answer.style.display = 'none';
+  answer.style.display = 'block';
 }
+
+// contact form
+const form = document.querySelector('#contact form');
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(form);  
+
+  try {
+    const response = await fetch('https://formspree.io/f/mgveagne', {
+      method: 'POST',
+      body: formData,
+      redirect: 'manual'
+    });
+    
+    if (response.ok || response.type === 'opaqueredirect') {
+      alert('Message sent successfully!');
+      form.reset();
+      return
+    }
+    alert('There was an error sending your message. Please try again.');
+  } catch (err) {
+    console.error('Error:', err);
+    alert('There was an error sending your message. Please try again.');
+  }
+})
