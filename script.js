a4c888dfcc2f039299fc2c0555c1b0adf88223ec
Muhammed Anshif <muhammedanshif236@gmail.com>
2024-11-06 22:05:58 +0530

    feat: add drop-down for small screen size in navbar

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+// nav
+const menuIcon = document.querySelector('nav .menu');
+const closeIcon = document.querySelector('nav .drop-down .close');
+const dropDownLinks = document.querySelectorAll('nav .drop-down div a');
+const menu = document.querySelector('nav .drop-down');
+const overlay = document.getElementById('overlay');
+
+function toggleMenu() {
+  menu.classList.toggle('active');
+  closeIcon.classList.toggle('active');
+  overlay.classList.toggle('active');
+}
+
+menuIcon.addEventListener('click', toggleMenu);
+closeIcon.addEventListener('click', toggleMenu);
+overlay.addEventListener('click', toggleMenu);
+dropDownLinks.forEach(link => link.addEventListener('click', toggleMenu));
+
 // faq
 function toggleAnswer(button) {
   const answer = button.nextElementSibling;  
